43d3b51aa12418abe11bb9490bfe299530e30e9a
Kevin Frank <frankevi@fh-brandenburg.de>
2017-06-12 20:18:41 +0200

    - Date- und TimePicker für die TodoDetailActivity umgesetzt

--- app/src/main/java/todolist/mad/vfh/kfrank/de/todolist/activities/TodoDetailActivity.java
+++ app/src/main/java/todolist/mad/vfh/kfrank/de/todolist/activities/TodoDetailActivity.java
@@ -1,15 +1,23 @@
 package todolist.mad.vfh.kfrank.de.todolist.activities;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TimePicker;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import todolist.mad.vfh.kfrank.de.todolist.R;
 import todolist.mad.vfh.kfrank.de.todolist.model.TodoItem;
@@ -49,6 +57,53 @@ public class TodoDetailActivity extends AppCompatActivity {
         dueDateView.setText(item.getDueDate() == null ? null : TodoItem.dueDateFormat.format(item.getDueDate()));
         doneView.setChecked(item.isDone());
         favouriteView.setChecked(item.isFavourite());
+
+        // set listener
+        dueDateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Date dueDate = TodoItem.dueDateFormat.parse(dueDateView.getText().toString());
+                    Calendar calendar = new GregorianCalendar();
+                    calendar.setTime(dueDate);
+                    showDatePickerDialog(calendar);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void showDatePickerDialog(final Calendar calendar) {
+        DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                showTimePickerDialog(calendar);
+            }
+        };
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dialog = new DatePickerDialog(this, listener, year, month, dayOfMonth);
+        dialog.show();
+    }
+
+    private void showTimePickerDialog(final Calendar calendar) {
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+                dueDateView.setText(TodoItem.dueDateFormat.format(calendar.getTime()));
+            }
+        };
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog dialog = new TimePickerDialog(this, listener, hourOfDay, minute, true);
+        dialog.show();
     }
 
     @Override
